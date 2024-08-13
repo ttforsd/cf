ee21c7e0942626632ae9b387376a52b3b3fc4eb9
Thomas Tse <thomas@Thomass-MacBook-Air-2.local>
2024-08-13 02:43:41 +0100

    Tue 13 Aug 2024 02:43:41 BST

--- 1995c.py
+++ 1995c.py
@@ -1,16 +1,28 @@
-from math import log2 as log, ceil 
+from math import log2 as log, ceil, inf
 
 def solver(arr):
     res = 0 
+    addition = [0] * len(arr) 
+    arr[0] = log(arr[0])
+    if arr[0] != 0: 
+        arr[0] = log(arr[0])
+    else: 
+        arr[0] = -inf 
     for i in range(1, len(arr)): 
-        if arr[i] >= arr[i - 1]:
+        arr[i] = log(arr[i])
+        if arr[i] != 0: 
+            arr[i] = log(arr[i])
+        else: 
+            arr[i] = -inf
+        if arr[i] >= arr[i - 1] + addition[i - 1]:
             continue 
-        elif arr[i] == 1: 
+        elif arr[i] == -inf: 
             return -1 
-        power = ceil(log(log(arr[i-1]) / log(arr[i])))
+        power = ceil(arr[i - 1] - arr[i]) + addition[i - 1]
+        addition[i] = power
         res += int(power) 
-        arr[i] = arr[i] ** (2 ** power)
     return res 
+
 
 cases = int(input())
 
@@ -22,12 +34,7 @@ for case in range(cases):
     tmp = solver(arr) 
     print(tmp) 
 
-
-# a >= b ^ n 
-# log a >= n log b
-# n = log b / log a
-
-
 # a <= b ^ (2 ^ n)
 # log a <= (2 ^ n) log b 
 # n = log2(log a / log b)
+# n = log(log(a)) - log(log(b))
